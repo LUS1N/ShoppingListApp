a00db33292810c7b3abbef316edcd773246b19e0
andy03 <r4pp3r_ul@yahoo.com>
2016-03-05 23:25:47 +0100

    add shop menu

---
+++ app/src/main/java/blelll/shoppinglist/ShopDialogFragment.java
@@ -0,0 +1,51 @@
+package blelll.shoppinglist;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import Model.Shop;
+import Model.Storage;
+
+/**
+ * Created by Andy on 05/03/2016.
+ */
+class ShopDialogFragment extends DialogFragment {
+        @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+         LayoutInflater inflater = getActivity().getLayoutInflater();
+
+
+         final View dialogView = inflater.inflate(R.layout.fragment_shop_dialog, null);
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(dialogView)
+                // Add action buttons
+                .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        EditText editText = (EditText) dialogView.findViewById(R.id.enterShopName);
+                        if (!((TextView) editText).getText().toString().isEmpty()) {
+                            Storage.getInstance().addShop(new Shop(editText.getText().toString()));
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ShopDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
