d93d2c52882c03cef11f48c76c4a79fbdd50ec2e
andy03 <r4pp3r_ul@yahoo.com>
2016-03-07 21:37:07 +0100

    remove shop

--- app/src/main/java/blelll/shoppinglist/ShopListDialogFragment.java
+++ app/src/main/java/blelll/shoppinglist/ShopListDialogFragment.java
@@ -8,6 +8,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +19,9 @@ import Model.Storage;
  * Created by Andy on 05/03/2016.
  */
 public class ShopListDialogFragment extends DialogFragment {
+
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -29,15 +33,15 @@ public class ShopListDialogFragment extends DialogFragment {
         // Pass null as the parent view because its going in the dialog layout
 
         ArrayList<Shop> shops = Storage.getInstance().getShops();
-        ArrayList<String> shopsName = new ArrayList<>();
+        final ArrayList<String> shopsName = new ArrayList<>();
         for(Shop shop :shops)
             shopsName.add(shop.toString());
 
         builder.setTitle(R.string.get_shops)
                 .setItems(shopsName.toArray(new String[shopsName.size()]), new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                // The 'which' argument contains the index position
-                                // of the selected item
+                                Shop removedShop = Storage.getInstance().getShop(shopsName.get(which));
+                               Storage.getInstance().removeShop(removedShop);
                             }
                         })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
